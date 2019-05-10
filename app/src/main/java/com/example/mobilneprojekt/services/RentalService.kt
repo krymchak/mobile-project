724260ed9fca844cc66c26664ad885e8c1aff75c
jaroslav20 <jarek.pokropinski@gmail.com>
2019-05-10 11:39:16 +0200

    Added changes to rest controller

--- app/src/main/java/com/example/mobilneprojekt/services/RentalService.kt
+++ app/src/main/java/com/example/mobilneprojekt/services/RentalService.kt
@@ -3,15 +3,24 @@ package com.example.mobilneprojekt.services
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface RentalService {
     @GET("sec/cars")
-    fun getCars(): Call<List<CarDTO>>
+    fun getCars(@Header("Authorization") token: String): Call<List<CarDTO>>
 
     @GET("sec/history")
-    fun getHistory(): Call<List<HistoryEntryDTO>>
+    fun getHistory(@Header("Authorization") token: String): Call<List<HistoryEntryDTO>>
+
+    @POST("sec/rent")
+    fun rentCar(@Header("Authorization") token: String, @Body carId: CarIdDTO): Call<Void>
+
+    @POST("sec/return")
+    fun returnCar(@Header("Authorization") token: String, @Body carId: CarIdDTO): Call<Void>
+    
 }
 
 data class CarDTO(val id: Int, val name: String, val year: String, val dmc: Int, val seats: Int, val mileage: Int, val category: String, val image: String)
 data class HistoryEntryDTO(val date: String, val id: Int, val name: String, val year: String, val dmc: Int, val seats: Int, val mileage: Int, val category: String, val image: String)
+data class CarIdDTO(val carId: Int)
