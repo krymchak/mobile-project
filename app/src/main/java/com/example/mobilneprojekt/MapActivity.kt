77dc1c4bfab2cefbf1ff328ad254264d7ad65c8c
MykolaMedynskyi <mykola.medynskyi@gmail.com>
2019-05-17 11:48:09 +0200

    add markers to map

---
+++ app/src/main/java/com/example/mobilneprojekt/MapActivity.kt
@@ -0,0 +1,221 @@
+package com.example.mobilneprojekt
+
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.location.Location
+import android.location.LocationManager
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import android.support.annotation.DrawableRes
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
+import android.util.DisplayMetrics
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Toast
+import com.example.mobilneprojekt.services.CarDTO
+import com.example.mobilneprojekt.services.ServiceBuilder
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import de.hdodenhof.circleimageview.CircleImageView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener{
+
+    private var service: LocationManager? = null
+    private var enabled: Boolean? = null
+    private var mLocationRequest: LocationRequest? = null
+    private var mGoogleApiClient: GoogleApiClient? = null
+    private var mLastLocation: Location? = null
+    private var firtLocation = false
+    private lateinit var mMap: GoogleMap
+    private var REQUEST_LOCATION_CODE = 101
+
+    lateinit var mapFragment: SupportMapFragment
+    lateinit var googleMap: GoogleMap
+
+    var listOfCars = ArrayList<CarDTO>()
+
+    override fun onLocationChanged(location: Location?) {
+        mLastLocation = location
+
+        if (!firtLocation) {
+            val latLng = LatLng(location!!.latitude, location.longitude)
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12f))
+            firtLocation = true
+        }
+    }
+
+    override fun onConnected(p0: Bundle?) {
+        mLocationRequest = LocationRequest()
+        mLocationRequest!!.interval = 1000
+        mLocationRequest!!.fastestInterval = 1000
+        mLocationRequest!!.priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+
+        if (!enabled!!) {
+            val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this)
+        }
+    }
+
+    override fun onConnectionSuspended(p0: Int) {
+    }
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.map_activity)
+        service = this.getSystemService(LOCATION_SERVICE) as LocationManager
+        enabled = service!!.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.gMap) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+    }
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                buildGoogleApiClient()
+                mMap.isMyLocationEnabled = true
+            } else {
+                checkLocationPermission()
+            }
+        } else {
+            buildGoogleApiClient()
+            mMap.isMyLocationEnabled = true
+        }
+
+        addMarkers()
+
+    }
+
+    @Synchronized
+    fun buildGoogleApiClient() {
+        mGoogleApiClient = GoogleApiClient.Builder(this)
+            .addConnectionCallbacks(this)
+            .addOnConnectionFailedListener(this)
+            .addApi(LocationServices.API)
+            .build()
+
+        mGoogleApiClient!!.connect()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_LOCATION_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        if (mGoogleApiClient == null) {
+                            buildGoogleApiClient()
+                        }
+                        mMap.isMyLocationEnabled = true
+                    }
+                } else {
+                    Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show()
+                }
+                return
+            }
+        }
+    }
+
+    private fun checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                AlertDialog.Builder(this)
+                    .setTitle("Location Permission Needed")
+                    .setMessage("This app needs the Location permission, please accept to use location functionality")
+                    .setPositiveButton("OK", DialogInterface.OnClickListener { dialog, which ->
+                        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION_CODE)
+                    })
+                    .create()
+                    .show()
+
+            } else ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION_CODE)
+        }
+    }
+
+    private fun addMarkers() {
+        val callCars = ServiceBuilder.getRentalService().getCars("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dpbiI6ImFkbWluIiwiaWF0IjoxNTU3NzQ2MjU0LCJleHAiOjE1NzA3MDYyNTR9.oaSsRbNO4vio9xkvEG70L-DcJ6LsDPaRyM_hxh3uAfU")
+        callCars.enqueue(object : Callback<List<CarDTO>> {
+            override fun onFailure(call: Call<List<CarDTO>>, t: Throwable) {
+                Log.e("call", "Failed to get list of cars")
+            }
+
+            override fun onResponse(call: Call<List<CarDTO>>, response: Response<List<CarDTO>>) {
+                Log.d("call", response.message())
+                val body = response.body()
+                if (body != null) {
+                    listOfCars.clear()
+                    listOfCars.addAll(body)
+                }
+            }
+        })
+
+        mapFragment = supportFragmentManager.findFragmentById(R.id.gMap) as SupportMapFragment
+        mapFragment.getMapAsync(OnMapReadyCallback {
+            googleMap = it
+
+            val from = LatLng(51.109687, 17.058089)
+            val to = LatLng(51.103508, 17.085291)
+
+            googleMap.addMarker(MarkerOptions().position(from).icon(BitmapDescriptorFactory.fromBitmap(createCustomMarker(this, R.drawable.s_marker))))
+            googleMap.addMarker(MarkerOptions().position(to).icon(BitmapDescriptorFactory.fromBitmap(createCustomMarker(this, R.drawable.f_marker))))
+
+        })
+
+    }
+
+    fun createCustomMarker(context: Context, @DrawableRes resource: Int): Bitmap {
+
+        val marker = (context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater).inflate(
+            R.layout.custom_marker_layout,
+            null
+        )
+
+        val markerImage = marker.findViewById(R.id.user_dp) as CircleImageView
+        markerImage.setImageResource(resource)
+
+        val displayMetrics = DisplayMetrics()
+        (context as Activity).windowManager.defaultDisplay.getMetrics(displayMetrics)
+        marker.layoutParams = ViewGroup.LayoutParams(52, ViewGroup.LayoutParams.WRAP_CONTENT)
+        marker.measure(displayMetrics.widthPixels, displayMetrics.heightPixels)
+        marker.layout(0, 0, displayMetrics.widthPixels, displayMetrics.heightPixels)
+        marker.buildDrawingCache()
+        val bitmap = Bitmap.createBitmap(marker.measuredWidth, marker.measuredHeight, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        marker.draw(canvas)
+
+        return bitmap
+    }
+
+}
